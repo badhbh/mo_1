e9b9be1a0fccc9c6299b10f8aa5faeaf78c44719
badhbh <annspb1@mail.ru>
2019-11-15 04:11:41 +0300

    moya oshibochka

--- gold.py
+++ gold.py
@@ -1,12 +1,14 @@
 import math
 
+
 def gold_method_min(a, b, input_func, accuracy):
     output = {'minimum': None, 'calc_counter': 0}
     phi = (1 + math.sqrt(5)) / 2
-    l = b - a
-    x1 = b - l/phi
-    x2 = a + l/phi
+   
     while True:
+        l = b - a
+        x1 = b - l/phi
+        x2 = a + l/phi
         if(input_func(x1) >= input_func(x2)):
             a = x1
         else:
@@ -16,5 +18,6 @@ def gold_method_min(a, b, input_func, accuracy):
         c = (a + b) / 2
         output['minimum'] = [c]
         output['calc_counter'] += 1
-    return output   
+    return output 
+    
 
